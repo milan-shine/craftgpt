e790f2cac200ca012d688e9766c37661784b5fda
adnanx07 <sadnans714@gmail.com>
2024-02-06 19:05:37 +0530

    feat: add breadcrumb component

--- components/breadcrumbs/BreadcrumbView.tsx
+++ components/breadcrumbs/BreadcrumbView.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { ReactNode } from "react";
 
@@ -7,15 +8,26 @@ export default function BreadcrumbView({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
   const routes = pathname.split("/").slice(2);
-  console.log(routes, "routes", pathname);
+  const isCurrentScreen = (index: number) =>  routes.length === index + 1
+  
   return (
-    <div className="flex flex-col">
-        <span className="flex">
-      {routes.map((path) => (
-        <span>/{path}</span>
-      ))}
+    <div className="flex flex-col pb-3">
+      <span className="flex">
+        {routes.map((path, i) =>
+           isCurrentScreen(i) ? (
+            <span key={path} className="text-muted-foreground">
+            {i > 0 && <span className="mx-2">/</span>}
+            {path}
+           </span>
+          ) : (
+            <Link key={path} className="hover:underline" href={`/admin/${path}`}>
+            {i > 0 && <span className="mx-2">/</span>}
+            {path.replace(/-/g, ' ')}
+          </Link>
+          )
+        )}
       </span>
       {children}
     </div>
-  );
+  )
 }
