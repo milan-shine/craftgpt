ce9dd661cb088ec620cb07c13773228225e312e6
VirajSamani2153 <shineinfosoft84@gmail.com>
2024-02-29 14:56:29 +0530

    fix: copy button disable

--- components/buttons/CopyToClipboardBtn.tsx
+++ components/buttons/CopyToClipboardBtn.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { CheckSquare, Copy } from 'lucide-react'
+import React, { useEffect } from "react";
+import { CheckSquare, Copy } from "lucide-react";
 
 export default function CopyButton({
   text,
@@ -15,19 +15,26 @@ export default function CopyButton({
     setIsCopied(true)
     }
 
-  useEffect(()=> {
+  useEffect(() => {
     setTimeout(() => {
-      setIsCopied(false)
-    },1500)
-  }, [isCopied])
-  return(
-    <div title="copy" className='cursor-pointer relative' onClick={copyText}>
-      {isCopied && <span className='absolute bg-black rounded-md text-sm text-white px-2' style={{marginTop: '-2rem'}}>Copied</span> }
-      {
-                isCopied
-                ? <CheckSquare/>
-                : <Copy/>
-            }
+      setIsCopied(false);
+    }, 1500);
+  }, [isCopied]);
+  return (
+    <div
+      title="copy"
+      className={`relative cursor-pointer ${disabled ? "" : "text-gray-400"}`}
+      onClick={disabled ? copyText : () => {}}
+    >
+      {isCopied && (
+        <span
+          className="absolute rounded-md bg-black px-2 text-sm text-white"
+          style={{ marginTop: "-2rem" }}
+        >
+          Copied
+        </span>
+      )}
+      {isCopied ? <CheckSquare /> : <Copy />}
     </div>
-  )
+  );
 }
